7d74667f26f153664f1e2e7c659089ae85ada44d
tuacy <1007178106@qq.com>
2020-03-14 18:02:48 +0800

    vant组件的使用
    vant组件的使用

--- miniprogram/pages/vant/vant.js
+++ miniprogram/pages/vant/vant.js
@@ -1,66 +1,68 @@
 // pages/vant/vant.js
+
+import Toast from '@vant/weapp/toast/toast';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    active: 0,
+    steps: [{
+        text: '步骤一',
+        desc: '描述信息'
+      },
+      {
+        text: '步骤二',
+        desc: '描述信息'
+      },
+      {
+        text: '步骤三',
+        desc: '描述信息'
+      },
+      {
+        text: '步骤四',
+        desc: '描述信息'
+      }
+    ],
+    show: true,
+    actions: [{
+        name: '选项'
+      },
+      {
+        name: '选项'
+      },
+      {
+        name: '选项',
+        subname: '副文本',
+        openType: 'share'
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onChange(event) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({
+      active: event.detail
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onClickIcon() {
+    Toast('点击图标');
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onClickButton() {
+    Toast('点击按钮');
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onClose() {
+    this.setData({
+      show: false
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onSelect(event) {
+    console.log(event.detail);
   }
 })
